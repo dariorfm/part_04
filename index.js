10b7ceba5330113e217eb6b751447bb92ca9bdd8
dariorfm <dariorfm@gmail.com>
2024-05-02 15:42:04 +0200

    Express simple server created on port 3001

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('This is part 04 Madafakar!');
+const express = require('express');
+const app = express();
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, this is part 04 of FullstakOpen Course!</h1>');
+});
+
+
+
+const port = 3001;
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    });
+
