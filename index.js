78a4501df9df27c8d5029f61c5f5f3f5f4bcf548
dariorfm <dariorfm@gmail.com>
2024-05-06 15:03:30 +0200

    Moving DB settings to its own module, file blog.js

--- index.js
+++ index.js
@@ -2,26 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-.then(() => {
-  console.log('connected to MongoDB')
-})
-.catch((error) => {
-  console.log('error connecting to MongoDB:', error.message)
-})
-
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 
 app.use(cors())
