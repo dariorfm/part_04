50f472e218dd212382c9c472072dfc152119a825
dariorfm <dariorfm@gmail.com>
2024-05-06 14:47:29 +0200

    New DB created and connected to app. POST and GET requests tested with Insomnia

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+.then(() => {
+  console.log('connected to MongoDB')
+})
+.catch((error) => {
+  console.log('error connecting to MongoDB:', error.message)
+})
 
 
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+
+app.use(cors())
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello, this is part 04 of FullstakOpen Course! Now, if I make a change it could be updated only restarting de web browser.</h1>');
 });
 
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
 
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
 
-const port = 3001;
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-    });
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
 
