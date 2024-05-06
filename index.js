dbcb597b7db27ab573e430895b99eba434a3e998
dariorfm <dariorfm@gmail.com>
2024-05-06 16:14:01 +0200

    4.2 Blog list step_2: App refactorated

--- index.js
+++ index.js
@@ -1,39 +1,7 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const cors = require('cors')
-const Blog = require('./models/blog')
+const app = require('./app') 
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-
-app.use(cors())
-app.use(express.json())
-
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello, this is part 04 of FullstakOpen Course! Now, if I make a change it could be updated only restarting de web browser.</h1>');
-});
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
-
