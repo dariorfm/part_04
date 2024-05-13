7615e535c13acccc13a3419431fe850ef24c1c50
dariorfm <dariorfm@gmail.com>
2024-05-13 13:15:41 +0200

    4.5 step_3 favoriteBlog function and tests defined

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined
+  } else if (blogs.length === 1) {
+    return blogs[0]
+  }
+  return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog, blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
