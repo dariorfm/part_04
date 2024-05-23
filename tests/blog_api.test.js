96aba6046e327e790493adc9875ff69be2c94af0
dariorfm <dariorfm@gmail.com>
2024-05-23 12:21:11 +0200

    4.10 _step_3 est that verifies that the unique identifier property of the blog posts is named id

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,15 +1,25 @@
 const { test, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async t => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('blog posts have id property', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+
+  blogs.forEach(blog => {
+    assert(blog.id)
+  })
 })
 
 after(async () => {
