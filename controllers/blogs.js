69b718b0d282c78c9a9a2992248623123713eed5
dariorfm <dariorfm@gmail.com>
2024-05-23 14:14:52 +0200

    4.10_step_3 the /api/blogs URL successfully creates a new blog post / POST operation refactored

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -28,14 +28,21 @@ blogsRouter.delete('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0
+  })
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,10 +1,36 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
+const Blog = require('../models/blog')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'Blog 1',
+    author: 'Author 1',
+    url: 'http://blog1.com',
+    likes: 1
+  },
+  {
+    title: 'Blog 2',
+    author: 'Author 2',
+    url: 'http://blog2.com',
+    likes: 2
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -20,6 +46,28 @@ test('blog posts have id property', async () => {
   blogs.forEach(blog => {
     assert(blog.id)
   })
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Blog 3',
+    author: 'Author 3',
+    url: 'http://blog3.com',
+    likes: 3
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(r => r.title)
+
+  assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+  assert(titles.includes('Blog 3'))
 })
 
 after(async () => {
